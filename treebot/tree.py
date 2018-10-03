d5ad1357e2cf9d380292726fcd0d72f3a0333cfa
Tim Hubner <thubner@instagram.com>
2018-10-03 12:15:31 -0700

    newne
    newnew

--- treebot/tree.py
+++ treebot/tree.py
@@ -4,7 +4,10 @@ TREE = yaml.load("""
 say: "Welcome to the Cult Institute. Do you feel a vague sense of dread?"
 answers:
   i think so:
-    say: https://media.giphy.com/media/4pMX5rJ4PYAEM/giphy.gif
+    say: hey hey ho ho. would you like to start over?
+    answers:
+		yes
+		no
   not really?:
     say: How long are you going to stay in the UK? up to 6 months; more than 6 months
     answers:
