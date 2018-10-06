80a40c3acb831b8e2fece05cd60debcf4c2c483b
Tim Hubner <thubner@instagram.com>
2018-10-05 18:06:50 -0700

    NEW
    NEW damnit

--- treebot/tree.py
+++ treebot/tree.py
@@ -3,16 +3,14 @@ import yaml
 TREE = yaml.load("""
 say: "Welcome to the Cult Institute. Do you feel a vague sense of dread?"
 answers:
-  Great!:
-    say: Please try again later when you’re filled with existential dread.
-  Just okay . . .:
-    say: Do you ever feel like something is, how shall I put this, not quite right?
+  i think so:
+    say: https://media.giphy.com/media/4pMX5rJ4PYAEM/giphy.gif
+  not really?:
+    say: How long are you going to stay in the UK? up to 6 months; more than 6 months
     answers:
-      I guess so?:
-        say: Are you ready to further explore this uncertainty and unveil your mithras?
-      Sometimes.:
-        say: I’m sensing denial. Denial is a tool taught to us by the masses to dull the truths of our pain. Are you sure you only sometimes feel like something isn’t quite right?
-      All the damn time.:
-		say: I can sense you’re ready for the next step. Don’t hesitate to call us today at 810-666-CULT for further assistance.
+      up to 6 months:
+        say: You can apply for a Short-term Study Visa
+      more than 6 months:
+        say: You need a Study Visa (Tier 4)
   
 """)
